aa750186b9ea159053c9942ba18824ec84a60dcb
schaff <schaff.mathieu93@gmail.com>
2026-01-12 22:28:56 +0100

    test(auth): add auth service tests

--- src/tests/helpers/test-factories.ts
+++ src/tests/helpers/test-factories.ts
@@ -1,1 +1,35 @@
-purged from history
+import { users, profiles } from "../../db/schema";
+import { hash } from "argon2";
+
+import { testDb } from "../db.test.config";
+
+export async function createTestUser(data?: {
+  email?: string;
+  username?: string;
+  password?: string;
+}) {
+  const passwordHash =
+    data?.password ||
+    (await hash("azerty123", {
+      memoryCost: 65536,
+      timeCost: 3,
+      parallelism: 4,
+    }));
+
+  const [user] = await testDb
+    .insert(users)
+    .values({
+      email: data?.email || `test${Date.now()}@exemple.com`,
+      passwordHash,
+    })
+    .returning();
+
+  if (!user) throw new Error("couldn't create user");
+
+  // Créer le profil automatiquement
+  await testDb.insert(profiles).values({
+    userId: user.id,
+  });
+
+  return user;
+}
