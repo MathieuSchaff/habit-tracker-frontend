a2342c378e9f18e7fc6678be56c0a340e3cb0fc8
schaff <schaff.mathieu93@gmail.com>
2026-01-27 23:59:33 +0100

    style: add css variables, layers and dark theme

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,8 @@ import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { queryClient } from "./lib/queryClient";
 import { routeTree } from "./routeTree.gen";
-
+// STYLES
+import "./styles/index.css";
 const router = createRouter({
   routeTree,
   context: { queryClient },
