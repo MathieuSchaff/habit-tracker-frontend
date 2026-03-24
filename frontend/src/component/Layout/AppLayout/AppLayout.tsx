bd4b17097da8ad7bbc4cd991af9553f550b8c8e4
schaff <schaff.mathieu93@gmail.com>
2026-03-24 17:18:22 +0100

    style: switch to formal 'vous', tweak auth titles

--- frontend/src/component/Layout/AppLayout/AppLayout.tsx
+++ frontend/src/component/Layout/AppLayout/AppLayout.tsx
@@ -1,16 +1,20 @@
-import { Outlet } from '@tanstack/react-router'
+import { Outlet, useRouterState } from '@tanstack/react-router'
 import { Toaster } from 'sonner'
 
 import { BottomNav } from '../../BottomNav/BottomNav'
 import { Header } from '../Header/Header'
+
 export const AppLayout = () => {
+  const pathname = useRouterState({ select: (s) => s.location.pathname })
+  const isAuthRoute = ['/login', '/signup', '/verify-email', '/verify-pending'].includes(pathname)
+
   return (
     <div className="app-layout">
-      <Header />
-      <div className="content">
+      {!isAuthRoute && <Header />}
+      <div className={`content ${isAuthRoute ? 'content--auth' : ''}`}>
         <Outlet />
       </div>
-      <BottomNav />
+      {!isAuthRoute && <BottomNav />}
       <Toaster position="top-center" richColors />
     </div>
   )
